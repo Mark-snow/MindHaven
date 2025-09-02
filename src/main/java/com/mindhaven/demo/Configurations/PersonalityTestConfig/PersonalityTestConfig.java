5467fa669cc9c3703359d99b431393a056091702
Mark-snow <markchira55@gmail.com>
2025-09-02 10:57:01 +0300

    personality test questions

--- src/main/java/com/mindhaven/demo/Configurations/PersonalityTestConfig/PersonalityTestConfig.java
+++ src/main/java/com/mindhaven/demo/Configurations/PersonalityTestConfig/PersonalityTestConfig.java
@@ -1,48 +1,48 @@
 package com.mindhaven.demo.Configurations.PersonalityTestConfig;
 
-import java.util.Arrays;
-import java.util.List;
+// import java.util.Arrays;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
-import com.mindhaven.demo.Entities.MBTIAxis;
-import com.mindhaven.demo.Entities.PersonalityTest;
-import com.mindhaven.demo.Repositories.PersonalityTestRepository;
+// import com.mindhaven.demo.Entities.MBTIAxis;
+// import com.mindhaven.demo.Entities.PersonalityTest;
+// import com.mindhaven.demo.Repositories.PersonalityTestRepository;
 
-import jakarta.annotation.PostConstruct;
+// import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class PersonalityTestConfig {
 
-    @Autowired
-    private PersonalityTestRepository questionRepository;
+    // @Autowired
+    // private PersonalityTestRepository questionRepository;
 
-    @PostConstruct
-    public void initQuestions() {
-        List<PersonalityTest> questions = Arrays.asList(
-            // E/I PersonalityTests
-            new PersonalityTest("EI1", "I enjoy social gatherings", MBTIAxis.E_I, false),
-            new PersonalityTest("EI2", "I prefer small groups", MBTIAxis.E_I, true), // Reverse scored
-            new PersonalityTest("EI3", "I feel energized after interacting with people", MBTIAxis.E_I, false),
+    // @PostConstruct
+    // public void initQuestions() {
+    //     List<PersonalityTest> questions = Arrays.asList(
+    //         // E/I PersonalityTests
+    //         new PersonalityTest("EI1", "I enjoy social gatherings", MBTIAxis.E_I, false),
+    //         new PersonalityTest("EI2", "I prefer small groups", MBTIAxis.E_I, true), // Reverse scored
+    //         new PersonalityTest("EI3", "I feel energized after interacting with people", MBTIAxis.E_I, false),
 
-            // S/N PersonalityTests
-            new PersonalityTest("SN1", "I focus on concrete facts", MBTIAxis.S_N, true),
-            new PersonalityTest("SN2", "I enjoy abstract theories", MBTIAxis.S_N, false),
-            new PersonalityTest("SN3", "I imagine future possibilities often", MBTIAxis.S_N, false),
+    //         // S/N PersonalityTests
+    //         new PersonalityTest("SN1", "I focus on concrete facts", MBTIAxis.S_N, true),
+    //         new PersonalityTest("SN2", "I enjoy abstract theories", MBTIAxis.S_N, false),
+    //         new PersonalityTest("SN3", "I imagine future possibilities often", MBTIAxis.S_N, false),
 
-            // T/F PersonalityTests
-            new PersonalityTest("TF1", "I prioritize logic over feelings", MBTIAxis.T_F, true),
-            new PersonalityTest("TF2", "I consider others' emotions when deciding", MBTIAxis.T_F, false),
-            new PersonalityTest("TF3", "I value fairness more than harmony", MBTIAxis.T_F, true),
+    //         // T/F PersonalityTests
+    //         new PersonalityTest("TF1", "I prioritize logic over feelings", MBTIAxis.T_F, true),
+    //         new PersonalityTest("TF2", "I consider others' emotions when deciding", MBTIAxis.T_F, false),
+    //         new PersonalityTest("TF3", "I value fairness more than harmony", MBTIAxis.T_F, true),
 
-            // J/P PersonalityTests
-            new PersonalityTest("JP1", "I prefer structure and plans", MBTIAxis.J_P, true),
-            new PersonalityTest("JP2", "I like keeping my options open", MBTIAxis.J_P, false),
-            new PersonalityTest("JP3", "I make lists and schedules", MBTIAxis.J_P, true)
-        );
+    //         // J/P PersonalityTests
+    //         new PersonalityTest("JP1", "I prefer structure and plans", MBTIAxis.J_P, true),
+    //         new PersonalityTest("JP2", "I like keeping my options open", MBTIAxis.J_P, false),
+    //         new PersonalityTest("JP3", "I make lists and schedules", MBTIAxis.J_P, true)
+    //     );
 
-        questionRepository.saveAll(questions);
+    //     questionRepository.saveAll(questions);
 
-    }
+    // }
 }   
